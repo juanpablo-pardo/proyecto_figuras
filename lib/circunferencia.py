facb5e6328a9dc485999108832c403c803246f9f
Lucia Perez Moretti <luciaperezmoretti8@gmail.com>
2025-10-07 18:08:41 +0200

    Creadas las funciones para la circunferencia

---
+++ lib/circunferencia.py
@@ -0,0 +1,11 @@
+def get_area(radio:float)->float:
+    """Calcula el área de una circunferencia dado su radio.
+
+    Args:
+        radio (float): Radio de la circunferencia.
+
+    Returns:
+        float: Área de la circunferencia.
+    """
+    import math
+    return math.pi * radio ** 2
